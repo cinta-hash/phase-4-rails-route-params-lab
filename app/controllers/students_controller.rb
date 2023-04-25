5317218a8984ee6c9924ab81dd8f7de128ca8422
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-04-25 23:43:08 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student_id = Student.find(params[:id])
+    render json: student_id
+  end
 
 end
